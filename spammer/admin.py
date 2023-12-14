56e729c7d84e911f058bdbafb40079085734eb61
LudaKend <Ludakend@gmail.com>
2023-12-14 09:04:37 +0300

    админка

--- spammer/admin.py
+++ spammer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from spammer.models import Spammer
 
-# Register your models here.
+
+@admin.register(Spammer)
+class SpammerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'spammer_name', 'company', 'email', 'is_active')
+    list_filter = ('spammer_name', 'is_active',)
+    search_fields = ('spammer_name',)
--- spammer/models.py
+++ spammer/models.py
@@ -6,12 +6,13 @@ class Spammer(models.Model):
     '''класс-модель для пользователей-менеджеров, формирующих рассылки'''
     spammer_name = models.CharField(max_length=100, verbose_name='ФИО менеджера')
     company = models.TextField(verbose_name='Компания', **NULLABLE)
+    email = models.EmailField(verbose_name='email менеджера', **NULLABLE)
     is_active = models.BooleanField(verbose_name='действителен', default=True)
 
 
     def __str__(self):
         '''строковое отображение обьекта'''
-        return f'{self.name}, {self.company}, {self.is_active}'
+        return f'{self.spammer_name}, {self.company}, {self.is_active}'
 
     class Meta:
         verbose_name = 'Менеджер'
