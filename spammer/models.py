3a1c4dd839f43e61e2cf7db0be08705b5689909f
LudaKend <Ludakend@gmail.com>
2023-12-14 10:47:14 +0300

    клиенты принадлежат менеджерам

--- spammer/models.py
+++ spammer/models.py
@@ -2,6 +2,24 @@ from django.db import models
 
 NULLABLE = {'null': True, 'blank': True}
 
+
+class Client(models.Model):
+    '''класс-модель для получателей рассылок'''
+    client_name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    email = models.EmailField(verbose_name='email клиента', **NULLABLE)
+    spammer = models.ForeignKey('spammer.Spammer', on_delete=models.CASCADE, verbose_name='ФИО менеджера', **NULLABLE)
+    is_active = models.BooleanField(verbose_name='действителен', default=True)
+
+
+    def __str__(self):
+        '''строковое отображение обьекта'''
+        return f'{self.client_name}, {self.is_active}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
 class Spammer(models.Model):
     '''класс-модель для пользователей-менеджеров, формирующих рассылки'''
     spammer_name = models.CharField(max_length=100, verbose_name='ФИО менеджера')
@@ -17,20 +35,4 @@ class Spammer(models.Model):
     class Meta:
         verbose_name = 'Менеджер'
         verbose_name_plural = 'Менеджеры'
-
-
-class Client(models.Model):
-    '''класс-модель для получателей рассылок'''
-    client_name = models.CharField(max_length=100, verbose_name='Имя клиента')
-    email = models.EmailField(verbose_name='email клиента', **NULLABLE)
-    is_active = models.BooleanField(verbose_name='действителен', default=True)
-
-
-    def __str__(self):
-        '''строковое отображение обьекта'''
-        return f'{self.client_name}, {self.is_active}'
-
-    class Meta:
-        verbose_name = 'Клиент'
-        verbose_name_plural = 'Клиенты'
 
