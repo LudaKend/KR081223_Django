3d0345b2dea90c475a0b0793607908c4b4a365e1
LudaKend <Ludakend@gmail.com>
2023-12-18 21:40:57 +0300

    доступ к  mail и продолжаем с принудительной рассылкой

--- mailing/management/commands/manual_send.py
+++ mailing/management/commands/manual_send.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 from mailing.models import Mailing
+from spammer.models import Spammer, Client
 from django.core.mail import send_mail
 
 class Command(BaseCommand):
@@ -8,9 +9,19 @@ class Command(BaseCommand):
         send_list = Mailing.objects.all()
         print(send_list) # для отладки
 
-        # for item in send_list:
-        #     if item.
+        send_list = Mailing.objects.filter(status='2')
+        print(send_list)  # для отладки
+
+        for item in send_list:
+            send_mail(subject=Mailing.subject, message=Mailing.mailing_text, from_email=Spammer.email, recipient_list=Client.email)
         #     print(item)
+        #     print(item.status)
+        #     print(item.spammer)
+        #     if item.status == 'в работе':
+        #         spammer = item.spammer
+        #         print(spammer)
+        #         print(item.spammer)
+
         #     Category.objects.create(**item)
         # category_for_create = []
         # for item in list_category:
